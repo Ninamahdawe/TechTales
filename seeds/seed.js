e66cba150afd03d132441b8bd9cf7615d2660787
Ninamahdawe <ninamahdawe11@gmail.com>
2023-09-22 16:10:43 -0700

    added the seeds folder

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,3 +2,23 @@ const fs = require('fs');
 const path = require('path');
 const sequelize = require('../config/connection');
 const { Project } = require('../models');
+
+const projectData = JSON.parse(
+    fs.readFileSync(path.join(__dirname, 'projectData.json'), 'utf-8')
+);
+const seedDatabase = async () => {
+    try {
+        await sequelize.sync({ force: true });
+
+        await Project.bulkCreate(projectData);
+
+        console.log('Database seeded successfully!');
+    } catch (error) {
+        console.error('Error seeding database:', error);
+    }
+
+    process.exit(0);
+};
+
+// Call the seedDatabase function
+seedDatabase();
