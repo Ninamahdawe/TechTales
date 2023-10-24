815806d9b86b0419de86ccf3f5e2aaf6c6f227be
Ninamahdawe <ninamahdawe11@gmail.com>
2023-10-23 22:29:51 -0700

    debugging index.js file

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@
 const router = require('express').Router();
 
 
-const homeRoutes = require('./homeroutes')
+const homeRoutes = require('./homeRoutes')
 const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
